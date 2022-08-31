9ab2ed8ef97a223c62e037fd1aabd3a73a96f311
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2022-08-31 16:53:08 +0100

    [C] Update `sieve`

--- c/sieve/sieve.c
+++ c/sieve/sieve.c
@@ -6,33 +6,32 @@
 
 uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes)
 {
-   if (!primes)
-   {
-      return EXIT_FAILURE;
-   }
+    /* Initialize boolean mask array */
+    bool *is_prime = malloc((limit + 1) * sizeof(bool));
+    if (!is_prime)
+    {
+        return 0;
+    }
+    memset(is_prime, true, (limit + 1) * sizeof(bool));
 
-   /* Initialize boolean mask array */
-   bool is_prime[limit + 1];
-   memset(is_prime, true, (limit + 1)*sizeof(*is_prime));
+    uint32_t primes_idx = 0;
+    for (uint32_t p = 2; (p <= limit) && (primes_idx < max_primes); primes_idx++)
+    {
+        /* Mark multiples of `p` as composite */
+        for (uint32_t k = p; (p * k) <= limit; k++)
+        {
+            is_prime[p * k] = false;
+        }
 
-   uint32_t i = 0;
-   for (uint32_t p = 2, k; p <= limit && i < max_primes; i++)
-   {
-      /* Mark multiples of `p` as composite */
-      k = p;
-      while ((p * k) <= limit)
-      {
-         is_prime[p * (k++)] = false;
-      }
+        /* Add prime `p` to `primes` array */
+        primes[primes_idx] = p++;
 
-      /* Add prime `p` to `primes` array */
-      primes[i] = p++;
-
-      /* Find the next prime `p` */
-      while (!is_prime[p])
-      {
-         ++p;
-      }
-   }
-   return limit < 2 ? 0 : i;
+        /* Find the next prime `p` */
+        while ((p <= limit) && (!is_prime[p]))
+        {
+            ++p;
+        }
+    }
+    free(is_prime);
+    return primes_idx;
 }
