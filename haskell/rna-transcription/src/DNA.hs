5f929ea696c0d62680ecd400f79973f9ef826a59
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-05-06 16:52:26 -0700

    [Haskell] Refactor `rna-transcription`

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,20 +1,11 @@
 module DNA (toRNA) where
 
-import qualified Data.Map   as Map
-import           Data.Maybe (fromMaybe)
-import qualified Data.Set   as Set
-
-
-nucleotideMap :: Map.Map Char Char
-nucleotideMap = Map.fromList [('G', 'C'), ('C', 'G'), ('T', 'A'), ('A', 'U')]
-
-correspondingNucleotide :: Char -> Char
-correspondingNucleotide c = fromMaybe c (Map.lookup c nucleotideMap)
-
 toRNA :: String -> Either Char String
-toRNA xs
-  | isInvalidSequence = Left $ head . dropWhile (`Map.member` nucleotideMap) $ xs
-  | otherwise         = Right transcribedRNA
-  where generateDNA x acc   = correspondingNucleotide x : acc
-        transcribedRNA      = foldr generateDNA [] xs
-        isInvalidSequence   = not . null $ Set.fromList xs Set.\\ Map.keysSet nucleotideMap
+toRNA = traverse fromDNA
+  where
+    fromDNA c
+      | c == 'G' = Right 'C'
+      | c == 'C' = Right 'G'
+      | c == 'T' = Right 'A'
+      | c == 'A' = Right 'U'
+      | otherwise = Left c
