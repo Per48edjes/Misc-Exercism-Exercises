12d319d16480682d377a218bb7c8473e4455eb8c
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-05-24 21:15:41 -0700

    [Haskell] Refactor `nucleotide-count`

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,15 +1,30 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module DNA (nucleotideCounts, Nucleotide (..)) where
 
-import Data.Map (Map, adjust, fromList)
+import Data.Map (Map, fromList, fromListWith, toList)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-updateCounter :: Char -> Either String (Map Nucleotide Int) -> Either String (Map Nucleotide Int)
-updateCounter 'A' (Right m) = Right $ adjust (+ 1) A m
-updateCounter 'C' (Right m) = Right $ adjust (+ 1) C m
-updateCounter 'G' (Right m) = Right $ adjust (+ 1) G m
-updateCounter 'T' (Right m) = Right $ adjust (+ 1) T m
-updateCounter _ _ = Left "Invalid nucleotide"
+newtype NucleotideCounter = NucleotideCounter (Either String (Map Nucleotide Int))
+    deriving (Eq, Show)
+
+updateCounter :: Char -> NucleotideCounter
+updateCounter 'A' = NucleotideCounter $ Right $ fromList [(A, 1)]
+updateCounter 'C' = NucleotideCounter $ Right $ fromList [(C, 1)]
+updateCounter 'G' = NucleotideCounter $ Right $ fromList [(G, 1)]
+updateCounter 'T' = NucleotideCounter $ Right $ fromList [(T, 1)]
+updateCounter _ = NucleotideCounter $ Left "Invalid nucleotide"
+
+instance Semigroup NucleotideCounter where
+    (NucleotideCounter (Right m1)) <> (NucleotideCounter (Right m2)) = NucleotideCounter (Right $ fromListWith (+) (toList m1 ++ toList m2))
+    (NucleotideCounter (Left s)) <> _ = NucleotideCounter (Left s)
+    _ <> (NucleotideCounter (Left s)) = NucleotideCounter (Left s)
+
+instance Monoid NucleotideCounter where
+    mempty = NucleotideCounter $ Right $ fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts = foldr updateCounter $ Right (fromList [(A, 0), (C, 0), (G, 0), (T, 0)])
+nucleotideCounts = getCounter . foldMap updateCounter
+  where
+    getCounter (NucleotideCounter c) = c
