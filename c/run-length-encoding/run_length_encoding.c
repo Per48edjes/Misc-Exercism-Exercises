39e6ef76d53a3641cd43547ef1aa562129c9ea58
Samuel Selleck <sellecksamuel@gmail.com>
2023-09-05 15:43:32 -0700

    decoder works! <3

--- c/run-length-encoding/run_length_encoding.c
+++ c/run-length-encoding/run_length_encoding.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 static size_t decimal_int_to_strlen(size_t n)
 {
@@ -26,14 +27,14 @@ static size_t decimal_int_to_str(size_t n, char* position)
     return count;
 }
 
-static void compressed_length_callback(void* env, size_t count, char letter)
+static void encoded_length_callback(void* env, size_t count, char letter)
 {
     (void)letter;
     size_t* compressed_length = env;
     *compressed_length += count == 1 ? 1 : (decimal_int_to_strlen(count) + 1);
 }
 
-static void writer_callback(void* env, size_t count, char letter)
+static void encoded_writer_callback(void* env, size_t count, char letter)
 {
     char** writer_head = env;
     if (count > 1)
@@ -45,7 +46,7 @@ static void writer_callback(void* env, size_t count, char letter)
     (*writer_head)++;
 }
 
-static void run_length_visitor(const char* text, void* env,
+static void decoded_run_length_visitor(const char* text, void* env,
                                void (*callback)(void* env, size_t count,
                                                 char letter))
 {
@@ -82,13 +83,68 @@ char* encode(const char* text)
     else
     {
         size_t compressed_length = 0;
-        run_length_visitor(text, &compressed_length,
-                           compressed_length_callback);
+        decoded_run_length_visitor(text, &compressed_length,
+                           encoded_length_callback);
         encoded_text = calloc((1 + compressed_length), sizeof(char));
         char* writer_head = encoded_text;
-        run_length_visitor(text, &writer_head, writer_callback);
+        decoded_run_length_visitor(text, &writer_head, encoded_writer_callback);
     }
     return encoded_text;
 }
 
-char* decode(const char* data);
+
+static void decoded_length_callback(void* env, size_t count, char letter) {
+    
+    (void)letter;
+    size_t* decompressed_length = env;
+    *decompressed_length += count;
+}
+
+static void decoded_writer_callback(void* env, size_t count, char letter)
+{
+    char** writer_head = env;
+    for(char* bound = *writer_head + count;*writer_head < bound; (*writer_head)++) {
+        **writer_head = letter;
+    }
+}
+
+static void encoded_run_length_visitor(
+    const char* encoded_text, void* env,
+    void (*callback)(void* env, size_t count,char letter)
+    ) {
+    size_t count = 0;
+    for (const char* ptr = encoded_text; *ptr; ptr++)
+    {
+        char letter = *ptr;
+        if (isdigit(letter)) {
+            count = count*10 + (letter - '0');
+        }
+        else
+        {
+            callback(env, count != 0 ? count : 1, letter);
+            count = 0;
+        }
+    }
+}
+
+char* decode(const char* encoded_text) {
+    if (encoded_text == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    char* decoded_text;
+    if (encoded_text[0] == '\0')
+    {
+        decoded_text = calloc(1, sizeof(char));
+    }
+    else
+    {
+       size_t decompressed_length = 0;
+        encoded_run_length_visitor(encoded_text, &decompressed_length,
+                           decoded_length_callback);
+        decoded_text = calloc((1 + decompressed_length), sizeof(char));
+        char* writer_head = decoded_text;
+        encoded_run_length_visitor(encoded_text, &writer_head, decoded_writer_callback);
+    }
+    return decoded_text;
+}
